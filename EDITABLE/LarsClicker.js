b3f9c21f204598bd87bca1df041b585a53c40d7c
Deppresso-Exppresso <74264236+Deppresso-Exppresso@users.noreply.github.com>
2022-10-12 10:18:09 +0200

    edit

--- EDITABLE/LarsClicker.js
+++ EDITABLE/LarsClicker.js
@@ -16,9 +16,25 @@ var a3 = new Audio ("audio/Nons1.mp3")
 var a4 = new Audio ("audio/Nons2.mp3")
 var a5 = new Audio ("audio/Talk.mp3")
 
-function music() {
-var rng = (Math.floor(Math.random() * 100))
-}
+var ranges = [
+    { divider: 1e21 , suffix: 'Sx'},
+    { divider: 1e18 , suffix: 'Qi' },
+    { divider: 1e15 , suffix: 'Qu' },
+    { divider: 1e12 , suffix: 'T' },
+    { divider: 1e9 , suffix: 'B' },
+    { divider: 1e6 , suffix: 'M' },
+    { divider: 1e3 , suffix: 'k' }
+  ];
+  
+  function formatNumber(n) {    
+    for (var i = 0; i < ranges.length; i++) {
+      if (n >= ranges[i].divider) {
+        return (n / ranges[i].divider).toFixed(2).toString() + ranges[i].suffix;
+        
+    }
+    }
+    return n.toString();
+  }
 
 function showbuildings() {
     document.getElementById("buildhud").style.zIndex = 1;
@@ -37,15 +53,13 @@ function showupgrades() {
 
 function update() {
     mælkC = Math.floor(mælk)
-    document.getElementById("amount").innerHTML = "Du har " + mælkC + " " + " mælk."
-    document.getElementById("MPS").innerHTML = "Du laver " + MPS + "  mælk hvert sekund."
-    console.log(mælk)
+    document.getElementById("amount").innerHTML = "Du har " + formatNumber(mælkC) + " " + " mælk."
+    document.getElementById("MPS").innerHTML = "Du laver " + formatNumber(MPS) + "  mælk hvert sekund."
     document.title = "Mælk = " + mælkC
 }
 
 //On click of "lars"
 function LarsClick() {
-    music()
     mælk += MPC
     MPS++
     click.play();
@@ -74,7 +88,9 @@ function slaver() {
 if(mælk >= slavecurrentprice) {
 slaveamount++
 mælk - slavecurrentprice
-MPS += 100
+MPS += 1
+slavecurrentprice = slavestartprice += slavestartprice * slavepercent;
+console.log(slavecurrentprice)
 document.getElementById("slaveP").innerHTML = "Koster " + slaveshownprice + " mælk"
 document.getElementById("slaveA").innerHTML = "Du har " + slaveamount + " slaver"
 update()
