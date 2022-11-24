eb984f41e0b10581e5588f3bcbbf634160e414f8
cocksucker42069 <fastboi2005@gmail.com>
2022-11-24 18:59:00 +0100

    i want to kill myself
    i do the code

--- LarsClicker.js
+++ LarsClicker.js
@@ -2,20 +2,21 @@ var mælk = 0 //Cookies
 var mælkC = 0 //Cookies to be displayed after converting to tons etc
 var MPS = 1.1 //Cookies per second
 var MPC = 1 //Cookies per click
-
+//mængden af dine arbejder
 var slaverM = 0
 var børnearbejdeM = 0
 var nseelevM = 0
 var landmandM = 0
 var nselærerM = 0
 var arlamalkeriM = 0
+//audio
 var click = new Audio("audio/buy1.mp3")
 var a1 = new Audio ("audio/Blather.mp3")
 var a2 = new Audio ("audio/JobJob.mp3")
 var a3 = new Audio ("audio/Nons1.mp3")
 var a4 = new Audio ("audio/Nons2.mp3")
 var a5 = new Audio ("audio/Talk.mp3")
-
+//konvertere mængder
 var ranges = [
     { divider: 1e21 , suffix: 'Sx'},
     { divider: 1e18 , suffix: 'Qi' },
@@ -35,7 +36,7 @@ var ranges = [
     }
     return n.toString();
   }
-
+  //knapper og hud
 function showbuildings() {
     document.getElementById("buildhud").style.zIndex = 1;
     document.getElementById("BB").style.backgroundColor = "#bfbfbf"
@@ -76,32 +77,81 @@ var Mps = window.setInterval(function(){
 
 // Upgrades //
 
-var slaveamount = 0;
-var slavepercent = 0.1;
-var slavestartprice = 10;
-var slavecurrentprice = slavestartprice += slavestartprice * slavepercent;
-var slaveshownprice = Math.floor(slavecurrentprice)
-
-
-
+//variabler for slaver 
+var slave_amount = 0;
+var slave_percent = 0.1;
+var slave_startprice = 15;
+var slave_currentprice = slave_startprice += slave_startprice * slave_percent;
+var slave_shownprice = Math.floor(slave_currentprice)
+//variabler for børnearbejde
+var børnearbejde_amount = 0;
+var børnearbejde_percent = 0.15;
+var børnearbejde_startprice = 100;
+var børnearbejde_currentprice = børnearbejde_startprice+= 
+børnearbejde_startprice * børnearbejde_percent;
+var børnearbejde_shownprice = math.floor(børnearbejde_currentprice);
+//variabler for nseelev
+var nseelev_amount = 0;
+var nseelev_percent = 0.25
+var nseelev_startprice = 500;
+var nseelev_currentprice = nseelev_startprice+=
+nseelev_startprice * nseelev_percent;
+var nseelev_shownprice = math.floor(nseelev_currentprice);
+//variabler for landmand
+var landmand_amount = 0;
+var landmand_percent = 0.30;
+var landmand_startprice = 5000;
+var landmand_currentprice = landmand_startprice+= 
+landmand_startprice * landmand_percent;
+var landmand_shownprice = math.floor(landmand_currentprice);
+//variabler for nselærer
+var nselærer_amount = 0;
+var nselærer_percent = 0.45;
+var nselærer_startprice = 50000;
+var nselærer_currentprice = nselærer_startprice+=
+nselærer_startprice * nselærer_percent;
+var math.floor(nselærer_currentprice);
+//variabler for arlamalkeri
+var arlamalkeri_amount = 0;
+var arlamalkeri_percent = 0.60;
+var arlamalkeri_startprice = 100000;
+var arlamalkeri_currentprice = arlamalkeri_startprice+= 
+arlamalkeri_startprice * arlamalkeri_percent;
+var math.floor(arlamalkeri_currentprice);
+//variabler for nordborgslots_efterskole 2.0
+var nordborgslots_efterskole_amount = 0;
+var nordborgslots_efterskole_percent = 0.75;
+var nordborgslots_efterskole_startprice = 100000000;
+var nordborgslots_efterskole_currentprice = nordborgslots_efterskole_startprice+=
+nordborgslots_efterskole_startprice * nordborgslots_efterskole_percent;
+var math.floor(nordborgslots_efterskole_currentprice);
+//funktioner
 function slaver() {
-if(mælk >= slavecurrentprice) {
-slaveamount++
-mælk - slavecurrentprice
+if(mælk >= slave_currentprice) {
+slave_amount++
+mælk = mælk - slave_currentprice
 MPS += 1
-slavecurrentprice = slavestartprice += slavestartprice * slavepercent;
-console.log(slavecurrentprice)
-document.getElementById("slaveP").innerHTML = "Koster " + slaveshownprice + " mælk"
-document.getElementById("slaveA").innerHTML = "Du har " + slaveamount + " slaver"
+slave_currentprice = slave_startprice += slave_startprice * slave_percent;
+console.log(slave_currentprice)
+document.getElementById("slaveP").innerHTML = "Koster " + slave_shownprice + " mælk"
+document.getElementById("slaveA").innerHTML = "Du har " + slave_amount + " slaver"
 update()
     }
 }
-
-function børnearbejde() {
-    if(mælk == 1) {
-        console.log("ok")
+  
+function børnearbejde(){
+if(mælk>= børnearbejde_currentprice){
+børnearbejde_amount++;
+mælk = mælk - børnearbejde_currentprice;
+MPS += 10;
+børnearbejde_currentprice = børnearbejde_startprice += 
+børnearbejde_startprice * børnearbejde_percent;
+console.log(børnearbejde_currentprice)
+document.getElementById("BørnearbejdereP").innerHTML = "Koster " + børnearbejde_shownprice + "mælk "
+document.getElementById("BørnearbejdereA").innerHTML = " Du har" + børnearbejde_amount + "Børnearbejdere "
+update()
     }
-}
+} 
 
 function nseelev() {
     if(mælk == 1) {
@@ -132,3 +182,4 @@ function nse() {
         console.log("ok")
     }
 }
+
